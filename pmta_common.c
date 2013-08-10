6cd449966ba62f0cbf22d0a39e40e76004d5f791
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-08-11 00:19:29 +0300

    Added missing pmta_common.c

---
+++ pmta_common.c
@@ -0,0 +1,12 @@
+/**
+ * @file pmta_common.c
+ * @brief Common functions and data structures — implementation
+ * @date Jul 4, 2013 v0.4
+ * @author Vladimir Kolesnikov <vladimir@extrememember.com>
+ */
+
+#include "pmta_common.h"
+
+PHP_FUNCTION(empty_destructor)
+{
+}
